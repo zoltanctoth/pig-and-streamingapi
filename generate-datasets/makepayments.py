2060751ba9b0cc2ffbcad57b065905c9866cb91c
Zoltan Toth <zoltan.toth@prezi.com>
2015-07-12 21:36:52 +0200

    make big data

--- generate-datasets/makepayments.py
+++ generate-datasets/makepayments.py
@@ -7,7 +7,7 @@ random.seed(2888)
 date = datetime.date(2015, 1, 1)
 # print "date,user_id,amount"
 while True:
-    user_id = random.randint(0, 100000)
+    user_id = random.randint(1, 100000)
     payment = random.randint(1, 4) * 50
 
     if random.randint(0, 30000) == 0:
--- generate-datasets/makeusers.py
+++ generate-datasets/makeusers.py
@@ -4,6 +4,6 @@ cities = ["Budapest", "Budapest", "Budapest", "Debrecen", "Debrecen", "Sopron", 
 random.seed(288)
 
 # print "user_id,city"
-for user_id in range(1, 1001):
+for user_id in range(1, 100000):
     city = cities[random.randint(0, len(cities) - 1)]
     print "%d,%s" % (user_id, city)
